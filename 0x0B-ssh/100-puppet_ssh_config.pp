869ded41b245821ec18d195d5b9ef5b1ed49bbd4
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-09-19 23:25:18 +0100

    config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,27 @@
 # configure ssh client
-$str = "Host * \n \tPasswordAuthentication no \n \tIdentityFile ~/.ssh/school 
-"
-$user = $facts['identity']['user']
+#$str = "Host * \n \tPasswordAuthentication no \n \tIdentityFile ~/.ssh/school 
+#"
+#$user = $facts['identity']['user']
 
-file { '~/.ssh/config':
-    ensure  => present,
-    path    => "/home/${user}/.ssh/config",
-    mode    => '0744',
-    owner   => $user,
-    group   => $user,
-    content => $str,
+#file { '~/.ssh/config':
+#    ensure  => present,
+#    path    => "/home/${user}/.ssh/config",
+#    mode    => '0744',
+#    owner   => $user,
+#    group   => $user,
+#    content => $str,
+#}
+
+file_line { 'Turn off passwd auth':
+      ensure  => present,
+      match   => "^PasswordAuthentication *",
+      path    => '/etc/ssh/sshd_conf',
+      line    => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+      ensure  => created,
+      match   => "^IdentityFile *",
+      path    => '/etc/ssh/sshd_conf',
+      line    => 'IdentityFile ~/.ssh/school',
 }
