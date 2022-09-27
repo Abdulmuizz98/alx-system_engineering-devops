8eb54ec9d594bba7e1a63c170e9fd062c27b56ba
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-09-27 12:38:12 +0100

    puppet config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -79,10 +79,11 @@ file { 'Create custom_404.html':
   path    => '/usr/share/nginx/html/custom_404.html',
   mode    => '0744',
   owner   => $user,
-  group   => $uer,
+  group   => $user,
   content => "Ceci n'est pas une page.\n\n",
 }
 
+$hostname = $facts['networking']['hostname']
 $header_line="\tserver_name _;\n\tadd_header X-Served-By $(hostname);\n"
 
 file_line { 'Configure custom 404 page':
@@ -90,7 +91,7 @@ file_line { 'Configure custom 404 page':
   notify  => Service['start nginx service'],
   path    => '/etc/nginx/sites-enabled/default',
   match   => '^\tserver_name\ _;',
-  line    => "${header_line}\n\terror_page 404 /custom_404.html;\n\tlocation = \
+  line    => "\tserver_name _;\n\tadd_header X-Served-By $(hostname);\n\n\terror_page 404 /custom_404.html;\n\tlocation = \
 /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}",
   replace => true,
 }
