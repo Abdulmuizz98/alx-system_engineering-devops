081be1f81024725dedfbcca3380fc0423d87d95b
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-10-18 00:28:41 +0100

    user and todo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ a Python script that, using the JSONPlaceholder  REST API,
 for a given employee ID, returns information about his/her
 TODO list progress.
 """
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
 
