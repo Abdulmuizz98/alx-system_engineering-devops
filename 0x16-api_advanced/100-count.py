3b9f7021aa3392a7a6eff6dd4682923fe9f50414
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-11-02 03:14:25 +0100

    count

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Contains a function that quries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given
+    subreddit.
+"""
+
+
+from requests import get
+
+
+def count_words(subreddit, word_list):
+    """
+    function that quries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given
+    subreddit.
+    """
+    header = {'User-Agent': 'my-app/0.0.1'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    req = get(url, headers=header)
+
+    if req.status_code == 200 and req.url == url:
+        dic = req.json().get('data').get('children')[:10]
+        for item in dic:
+            print(item.get('data').get('title'))
+    else:
+        print(None)
