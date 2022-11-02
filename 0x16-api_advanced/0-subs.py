ba49bf42f0468f83da7bbd2ec1e0170f7dd47ed2
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-11-02 01:16:39 +0100

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+"""
+    Scipt that requests the reddit api that finds the number
+    of subs of a subreddit
+"""
+
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """request the reddit api that finds the number
+        of subs of a subreddit.
+    """
+    header = {'User-Agent': 'my-app/0.0.1'}
+    req = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+              headers=header)
+
+    if req.json().get('kind') == 'Listing' or req.status_code != 200:
+        return (0)
+    else:
+        return (req.json().get('data').get('subscribers'))
